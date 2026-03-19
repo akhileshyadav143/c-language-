70e2d0f9ca10964d5b71091d1f7456c57e8f1d4d
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-03-19 14:44:20 +0530

    cpp

---
+++ DSA.cpp/code.cpp
@@ -0,0 +1,38 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Akhilesh yadav \n coder";
+//     return 0;
+// }
+
+
+//boilerplate code
+// #include<iostream>
+// using namespace std;
+// int main(){
+   
+//     return 0;
+// }
+
+
+//variable - containers to store data
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"";
+//     return 0;
+// }
+
+//data type
+#include<iostream>
+using namespace std;
+int main(){
+    int age=25;//int= 4byte
+    // cout<< age <<endl;
+    char grade='A';//char=1byte
+    float pi=3.14;//4byte
+    bool isSafe= false;
+    cout<<isSafe<<endl;
+    return 0;
+}
