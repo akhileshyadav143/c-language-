87ccba9d17956e25d73046e42390e824ebc54ea1
akhileshyadav143 <akhileshyadav667543@gmail.com>
2026-03-29 22:48:54 +0530

    dsa

---
+++ DSA.cpp/code3.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// using namespace std;
+// int summation(int n){
+// int sum=0;
+// for(int i=1;i<=n;i++)
+//     sum+=(i*i);
+//     return sum;
+// }
+
+// int main()
+// {
+//     int n=2;
+//     cout<< summation(n);
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int summition(int n){
+//     int sum=0;
+//     for(int i=0;i<=n;i++)
+//     sum+=(i*i);
+//     return sum;
+
+// }
+// int main(){
+//     int n=3;
+//     cout<<summition(n);
+//     return 0;
+// }
